b4c23b4494359ce053c16711d6180ff245f38718
Miguel Hinojoza <miguel.hinojoza23@gmail.com>
2023-11-30 17:44:12 -0600

    prueba cache 3

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
 const CACHE_NAME = 'cache-v1';
+const CACHE_DINAMICO = 'cachedin-v1';
+const CACHE_ESTATICO = 'cacheest-v1';
+const CACHE_INMUTABLE = 'cachein-v1';
 
 self.addEventListener('install', (event) => {
     const preCache = caches.open(CACHE_NAME)
@@ -7,15 +10,21 @@ self.addEventListener('install', (event) => {
                 'index.html',
                 'service.html',
                 'contact-us.html',
-                'css/style.css',
-                'css/bootstrap.min.css',
                 'js/app.js',
                 'offline.html',
                 'images/offline-imagen.jpg'
             ]);
         });
 
-    event.waitUntil(preCache);
+        const cacheInmutable = caches.open(CACHE_INMUTABLE)
+            .then(cache=>{
+                cache.addAll([
+                    'css/syle.css',
+                    'css/bootstrap.min.css',
+            ]);
+            });
+//Indicamos que la instalación espere hasta que las promesas se cumplan
+    event.waitUntil(preCache, cacheInmutable);
 });
 
 self.addEventListener('activate', (event) => {
@@ -40,16 +49,13 @@ self.addEventListener('fetch', (event) => {
                 if (response) {
                     return response;
                 }
-
                 console.log('No existe en cache', event.request.url);
-
                 return fetch(event.request)
                     .then((webResponse) => {
                         // Si la solicitud es para una página HTML y estamos offline, redirigir a offline.html
                         if (event.request.headers.get('accept').includes('text/html') && !navigator.onLine) {
                             return caches.match('offline.html');
                         }
-
                         return caches.open(CACHE_NAME)
                             .then((cache) => {
                                 cache.put(event.request, webResponse.clone());
@@ -59,7 +65,6 @@ self.addEventListener('fetch', (event) => {
             })
             .catch((error) => {
                 console.error('Fetch error:', error);
-
                 if (event.request.headers.get('accept').includes('text/html')) {
                     // Manejar la solicitud de la página HTML cuando hay un error de red
                     return caches.match('offline.html');
